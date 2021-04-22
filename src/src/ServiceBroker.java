0b5bade370a0aead4abf553175a74aca1ab7790d
HCurrence <46232898+HCurrence@users.noreply.github.com>
2021-04-21 21:20:06 -0400

    Works! Just working through the tests.

--- src/src/ServiceBroker.java
+++ src/src/ServiceBroker.java
@@ -3,11 +3,11 @@ import java.io.*;
 
 public class ServiceBroker {
 	
-	protected static String serviceFileLocation = "";
+	protected static String brokerFileLocation = "brokers/service.txt";
 	
 	public static void main(String[] args)
 	{
-		parseInstruction(args[0]);
+		System.out.println(parseInstruction("TRANSLATE,args"));
 	}
 	
 	public static String parseInstruction(String input)
@@ -21,53 +21,71 @@ public class ServiceBroker {
 		String returnData = "";
 		
 		Boolean foundFlag = false;
-		String utilityModuleLocation = "";
-		Scanner serviceFile = new Scanner(serviceFileLocation);
-		serviceFile.useDelimiter(",");
+		String utilityModule = "";
 		
-		do
+		try
 		{
-			//read service file codes for serviceCode
-			String code = serviceFile.next();
+			String brokerPath = new File(brokerFileLocation).getAbsolutePath();
+			File brokerFile = new File(brokerPath);
+			Scanner serviceFile = new Scanner(brokerFile);
+			serviceFile.useDelimiter(",");
 			
-			if(code.equals(serviceCode))
+			do
 			{
-				utilityModuleLocation = serviceFile.nextLine();
-				foundFlag = true;
-			}
-			
-		}while(foundFlag==false || serviceFile.hasNext()==false);
-		
-		if(foundFlag)
-		{
-			String serviceArgs = in.nextLine();
-			try
-			{
-				serviceArgs = serviceArgs.replace(',', ' ');
-				utilityModuleLocation = "java -jar " + utilityModuleLocation + " " + serviceArgs;
-				Process p = Runtime.getRuntime().exec(new String[]{utilityModuleLocation,serviceArgs});
-				BufferedReader moduleOutput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-					
-				String curr = "";
-				do
-				{
-					curr = moduleOutput.readLine();
-					returnData += curr;
-				}while(curr != null);
+				//read service file codes for serviceCode
+				String code = serviceFile.next();
 				
-				returnCode = 0;
-			} 
-			catch (IOException e)
+				if(code.equals(serviceCode))
+				{
+					utilityModule = serviceFile.nextLine().substring(1);
+					foundFlag = true;
+				}
+				
+			}while(foundFlag==false || serviceFile.hasNext()==false);
+			
+			if(foundFlag)
 			{
-				returnData = e.getMessage();
-				returnCode = 5;
+				String serviceArgs = in.nextLine();
+				try
+				{
+					serviceArgs = serviceArgs.substring(1).replace(',', ' ');
+					
+					String utilityPath = new File(utilityModule).getAbsolutePath();
+					
+					utilityModule = "java -jar " + utilityPath + " " + serviceArgs;
+					Process p = Runtime.getRuntime().exec(utilityModule);
+					BufferedReader moduleOutput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+						
+					String curr = "";
+					do
+					{
+						curr = moduleOutput.readLine();
+						returnData += curr;
+					}while(curr != null);
+					
+					returnCode = 0;
+				} 
+				catch (IOException e)
+				{
+					returnData = e.getMessage();
+					returnCode = 5;
+				}
 			}
+			else
+			{
+				returnData = parseInstruction("MESSAGE,408");
+				returnCode = 4;
+			}
+			
+			serviceFile.close();
 		}
-		else
+		catch(Exception e)
 		{
-			returnData = parseInstruction("MESSAGE,408");
+			returnData = parseInstruction("MESSAGE,401");
 			returnCode = 4;
 		}
+		
+		in.close();
 		
 		return returnCode + "," + returnData;
 	}
