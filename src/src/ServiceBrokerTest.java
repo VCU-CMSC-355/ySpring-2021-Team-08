ea4bf377a815357fc7f8c6ec1ba28bf6a315117e
HCurrence <46232898+HCurrence@users.noreply.github.com>
2021-04-27 11:57:19 -0400

    Update ServiceBrokerTest.java

--- src/src/ServiceBrokerTest.java
+++ src/src/ServiceBrokerTest.java
@@ -20,16 +20,48 @@ public class ServiceBrokerTest {
 	}*/
 	
 	@Test
-	public void test1() {
+	public void translateTest1() {
 		String input = "TRANSLATE,cat,spanish";
 		String expected = "0,gato";
 		String output = ServiceBroker.parseInstruction(input);
 		
 		assertEquals(expected, output);	
 	}
+	@Test
+	public void translateTest2() {
+		String input = "TRANSLATE,cat,german";
+		String expected = "0,katz";
+		String output = ServiceBroker.parseInstruction(input);
+		
+		assertEquals(expected, output);	
+	}
+	@Test
+	public void translateTest3() {
+		String input = "TRANSLATE,cat,french";
+		String expected = "0,chat";
+		String output = ServiceBroker.parseInstruction(input);
+		
+		assertEquals(expected, output);	
+	}
+	@Test
+	public void translateTest4() {
+		String input = "TRANSLATE,cat,klingon";
+		String expected = "0,mangHom";
+		String output = ServiceBroker.parseInstruction(input);
+		
+		assertEquals(expected, output);	
+	}
+	@Test
+	public void translateTest5() {
+		String input = "TRANSLATE,cat,english";
+		String expected = "0,cat";
+		String output = ServiceBroker.parseInstruction(input);
+		
+		assertEquals(expected, output);	
+	}
 	
 	@Test
-	public void test2() {
+	public void taxTest1() {
 		String input = "TAX,2020,Single,10000";
 		String expected = "0,2020 Single 10000";
 		String output = ServiceBroker.parseInstruction(input);
@@ -38,7 +70,7 @@ public class ServiceBrokerTest {
 	}
 	
 	@Test
-	public void test3() {
+	public void messageTest1() {
 		String input = "MESSAGE,404,language";
 		String expected = "0,404 language";
 		String output = ServiceBroker.parseInstruction(input);
