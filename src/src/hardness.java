a8581e74a375af102f8c5be5e056fb481a53e7a2
HCurrence <46232898+HCurrence@users.noreply.github.com>
2021-05-13 10:23:06 -0400

    Update hardness.java
    hardness.java can be used to test the serviceBroker.jar file.

--- src/src/hardness.java
+++ src/src/hardness.java
@@ -2,29 +2,47 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 
 public class hardness {
 	public static void main(String[] args) throws IOException {
 
+		Scanner scanner = new Scanner(System.in);
+		Boolean end = false;
+		
 		String utilityPath = new File("modules\\serviceBroker.jar").getAbsolutePath();
 		
-		utilityPath = "java -jar \"" + utilityPath + "\" TAX,2020,10000,S";
-		Process p = Runtime.getRuntime().exec(utilityPath);
-		BufferedReader moduleOutput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			
-		String output = "";
-		String curr = "";
-		while(curr != null)
+		while(!end)
 		{
-			curr = moduleOutput.readLine();
-			if(curr!=null)
+			System.out.println("Specify the Service Broker's command: ");
+			System.out.println("(Ex.: TAX,2020,10000,S | MESSAGE,401 | TRANSLATE,cat,german)");
+			String command = scanner.nextLine();
+			String utility = "java -jar \"" + utilityPath + "\" " + command;
+			Process p = Runtime.getRuntime().exec(utility);
+			BufferedReader moduleOutput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				
+			String output = "";
+			String curr = "";
+			while(curr != null)
 			{
-				output += curr;
+				curr = moduleOutput.readLine();
+				if(curr!=null)
+				{
+					output += curr;
+				}
+			}
+			
+			System.out.println(output);
+			p.destroy();
+			
+			System.out.println("End program? (y/n)");
+			command = scanner.nextLine();
+			if(command.equalsIgnoreCase("y") || command.equalsIgnoreCase("yes"))
+			{
+				end = true;
 			}
 		}
-		
-		System.out.println(output);
-		p.destroy();
+		scanner.close();
     }
 }
