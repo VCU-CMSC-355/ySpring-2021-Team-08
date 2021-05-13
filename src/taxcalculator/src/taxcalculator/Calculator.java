e2b91b69d98051ef08559ae5149310bb93ce3a23
MunimR <munim.rah123456@gmail.com>
2021-05-13 04:16:08 -0400

    updated taxcalc

--- src/taxcalculator/src/taxcalculator/Calculator.java
+++ src/taxcalculator/src/taxcalculator/Calculator.java
@@ -1,5 +1,8 @@
 package taxcalculator;
 import java.io.*;
+import java.util.ArrayList;
+
+//Format TaxCalculator.jar YEAR INCOME STATUS
 
 public class Calculator {
 	public static void main(String[] args) {
@@ -12,16 +15,35 @@ public class Calculator {
 			BufferedReader in = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream(args[0] + args[2] + ".txt"))); 
 			String s = null;
 			String[] data;
+			
+			ArrayList<Integer> max = new ArrayList<Integer>();
+			max.add(0);
+			ArrayList<Double> rate = new ArrayList<Double>();
+			rate.add(0.0);
+			
+			int left = Integer.parseInt(args[1]); 
+			double taxOwed = 0;
+			
 			while ((s = in.readLine())!=null) {
 				data = s.split(",");
-				if (data[0].equals("Over")) {
-					System.out.println(Integer.parseInt(args[1]) * Double.parseDouble(data[1]));
-				} else if (Integer.parseInt(args[1]) <= Integer.parseInt(data[0])) {
-					System.out.println(Integer.parseInt(args[1]) * Double.parseDouble(data[1]));
-					break;
-				}
+				if (data[0].equals("Over"))
+					max.add(Integer.MAX_VALUE);
+				else
+					max.add(Integer.parseInt(data[0]));
+				
+				rate.add(Double.parseDouble(data[1]));
 			}
+			
+			for (int i = 1; i < max.size() && left > 0; i++) {
+				double df = Math.min(max.get(i) - max.get(i - 1), left);
+				taxOwed += rate.get(i)*df;
+				left -= df;
+			}
+			
+			System.out.println(taxOwed);
+			
 		} catch (Exception e) {
+			System.out.println(e);
 			System.out.println("TABLE NOT FOUND");
 		}
 
